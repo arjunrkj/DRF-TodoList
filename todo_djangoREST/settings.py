389c0431b2ad9fcdce3cc0eaffa0aa92e3b82f71
Arjun <arjunkrishnan246@gmail.com>
2024-04-29 02:15:09 +0530

    production fixes

--- todo_djangoREST/settings.py
+++ todo_djangoREST/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-afow(a!_nu&j0=dl4poo2awq_egrua672x3%*gks9r^2@a^(ij'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
